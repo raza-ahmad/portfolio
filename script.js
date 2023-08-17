1e06b5c4275750a7818c05db6a0b841e3a2b47ff
Ahmad Raza <138312465+raza-ahmad@users.noreply.github.com>
2023-08-17 18:29:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// Typewriter Effect.
+
+var typeName = document.getElementById("name");
+var myName = ["Ahmad Raza", "A Frontend Developer", "A Web Designer"];
+let charPosition = 0;
+let textPosition = 0;
+
+typeWriter = () => {
+    typeName.innerText = myName[textPosition].substring(0, charPosition);
+    if(charPosition++ != myName[textPosition].length)
+    {
+        setTimeout(typeWriter, 100);
+    }
+    let text = myName[textPosition];
+    if(charPosition === text.length+1)
+    {
+        setTimeout(delChar, 3000);
+    }
+};
+
+delChar = () => {
+    typeName.innerText = myName[textPosition].substring(0, charPosition);
+    if(--charPosition >= 0)
+    {
+        setTimeout(delChar, 50);
+    }
+    if(charPosition === 0)
+    {
+        if(textPosition === myName.length-1)
+        {
+            textPosition = 0;
+        }
+        else
+        {
+            textPosition++;
+        }
+        setTimeout(typeWriter, 1000);
+    }
+};
+
+typeWriter();
+
+
+// Hamburger Nav.
+
+var collapse = document.getElementById("nav");
+var navItem = document.getElementsByClassName("nav-item");
+document.getElementById("hamburger").addEventListener("click", hameffect = () => {
+    collapse.classList.toggle("hamCollapse");
+    for(let i=0; i<navItem.length; i++){
+        navItem[i].classList.toggle("display");
+    }
+});
